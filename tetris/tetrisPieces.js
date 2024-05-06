e83f78e4e92230d520ad1a8a9a131bb11b142316
bagelfan <129016555+bagelfan@users.noreply.github.com>
2024-05-05 20:09:54 -0400

    Add files via upload

---
+++ tetris/tetrisPieces.js
@@ -0,0 +1,58 @@
+let tBlock = [[{row: 1, column: 4},{row: 1, column: 5}, {row: 0, column: 5}, {row: 1, column: 6}],
+    [{row: 1, column: 6},{row: 1, column: 5}, {row: 0, column: 5}, {row: 2, column: 5}],
+    [{row: 1, column: 4},{row: 1, column: 5}, {row: 2, column: 5}, {row: 1, column: 6}],
+    [{row: 1, column: 4},{row: 1, column: 5}, {row: 0, column: 5}, {row: 2, column: 5}]
+];
+let iBlock = [[{row: 1, column: 3},{row: 1, column: 4}, {row: 1, column: 5}, {row: 1, column: 6}],
+    [{row: 0, column: 5},{row: 1, column: 5}, {row: 2, column: 5}, {row: 3, column: 5}],
+    [{row: 2, column: 3},{row: 2, column: 4}, {row: 2, column: 5}, {row: 2, column: 6}],
+    [{row: 0, column: 4},{row: 1, column: 4}, {row: 2, column: 4}, {row: 3, column: 4}]
+];
+let oBlock = [[{row: 0, column: 4},{row: 0, column: 5},{row: 1,
+column: 4},{row: 1, column: 5}],
+    [{row: 0, column: 4},{row: 0, column: 5},{row: 1, column: 4},{row:
+1, column: 5}],
+    [{row: 0, column: 4},{row: 0, column: 5},{row: 1, column: 4},{row:
+1, column: 5}],
+    [{row: 0, column: 4},{row: 0, column: 5},{row: 1, column: 4},{row:
+1, column: 5}]
+];
+let jBlock = [[{row: 0, column: 4}, {row: 1, column: 5}, {row: 1,
+column: 4}, {row: 1, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 0, column: 6},
+{row: 2, column: 5}],
+    [{row: 2, column: 6}, {row: 1, column: 5}, {row: 1, column: 4},
+{row: 1, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 2, column: 4},
+{row: 2, column: 5}]
+];
+let lBlock = [[{row: 1, column: 4}, {row: 1, column: 5}, {row: 1,
+column: 6}, {row: 0, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 2, column: 6},
+{row: 2, column: 5}],
+    [{row: 2, column: 4}, {row: 1, column: 5}, {row: 1, column: 4},
+{row: 1, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 2, column: 5},
+{row: 0, column: 4}]
+];
+let sBlock = [[{row: 1, column: 4}, {row: 1, column: 5}, {row: 0,
+column: 5}, {row: 0, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 1, column: 6},
+{row: 2, column: 6}],
+    [{row: 2, column: 4}, {row: 1, column: 5}, {row: 2, column: 5},
+{row: 1, column: 6}],
+    [{row: 0, column: 4}, {row: 1, column: 5}, {row: 1, column: 4},
+{row: 2, column: 5}]
+];
+let zBlock = [[{row: 0, column: 4}, {row: 1, column: 5}, {row: 0,
+column: 5}, {row: 1, column: 6}],
+    [{row: 0, column: 6}, {row: 1, column: 5}, {row: 1, column: 6},
+{row: 2, column: 5}],
+    [{row: 1, column: 4}, {row: 1, column: 5}, {row: 2, column: 5},
+{row: 2, column: 6}],
+    [{row: 0, column: 5}, {row: 1, column: 5}, {row: 1, column: 4},
+{row: 2, column: 4}]
+];
+let triangleBlock = [[{row: 1, column: 4},{row: 1, column: 5}, {row: 0, column: 5}], [{row: 1, column: 4},{row: 1, column: 5}, {row: 0, column: 4}], [{row: 1, column: 4},{row: 0, column: 5}, {row: 0, column: 4}], [{row: 0, column: 4},{row: 1, column: 5}, {row: 0, column: 5}]]
+let twoBlock = [[{row: 0, column: 4}, {row: 0, column: 5}], [{row: 1, column: 5}, {row: 0, column: 5}], [{row: 1, column: 4}, {row: 1, column: 5}], [{row: 0, column: 4}, {row: 1, column: 4}]]
+let uBlock = [[{row: 1, column: 4}, {row: 1, column: 5}, {row: 1, column: 6}, {row: 0, column: 4}, {row: 0, column: 6}], [{row: 0, column: 5}, {row: 1, column: 5}, {row: 2, column: 5}, {row: 2, column: 6}, {row: 0, column: 6}], [{row: 1, column: 4}, {row: 1, column: 5}, {row: 1, column: 6}, {row: 2, column: 4}, {row: 2, column: 6}], [{row: 0, column: 5}, {row: 1, column: 5}, {row: 2, column: 5}, {row: 2, column: 4}, {row: 0, column: 4}]]
